2d8aaaba64e86d6c425d93f33a682b70e4dc73e4
LAPTOP-9D7D05BF\Shareem <shareemgelitoteofilo@yahoo.com>
2022-01-14 04:23:25 -0800

    Initial implementation of web scanning page

---
+++ app/src/main/java/com/example/webimagedownloader/webscanning/WebsiteScanningActivity.kt
@@ -0,0 +1,44 @@
+package com.example.webimagedownloader.webscanning
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.webimagedownloader.R
+
+class WebsiteScanningActivity : AppCompatActivity() {
+
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_website_scanning)
+
+        webView = findViewById(R.id.webViewToScan)
+
+        webView.webViewClient = WebViewClient()
+
+        // this will load the url of the website
+        webView.loadUrl("https://www.geeksforgeeks.org/")
+
+        // this will enable the javascript settings
+        webView.settings.javaScriptEnabled = true
+
+        // if you want to enable zoom feature
+        webView.settings.setSupportZoom(true)
+    }
+
+    // if you press Back button this code will work
+    override fun onBackPressed() {
+        // if your webview can go back it will go back
+        if (webView.canGoBack())
+            webView.goBack()
+        // if your webview cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
+    }
+}
