0e6545fc408de51d3ef5a4cc83e0377357649bb1
LAPTOP-9D7D05BF\Shareem <shareemgelitoteofilo@yahoo.com>
2022-01-14 21:38:41 -0800

    Update main screen

--- app/src/main/java/com/example/webimagedownloader/main/MainActivity.kt
+++ app/src/main/java/com/example/webimagedownloader/main/MainActivity.kt
@@ -15,10 +15,8 @@ import com.example.webimagedownloader.utils.Constants
 import com.example.webimagedownloader.utils.SharedPreferenceHelper
 import com.example.webimagedownloader.webscanning.WebsiteScanningActivity
 import com.google.android.material.textfield.TextInputEditText
-import java.io.File
 import android.provider.MediaStore
 import android.provider.DocumentsContract
-import com.example.webimagedownloader.utils.FileHelper
 import com.example.webimagedownloader.utils.UriUtils
 
 
@@ -27,7 +25,9 @@ import com.example.webimagedownloader.utils.UriUtils
 
 class MainActivity : AppCompatActivity() {
 
-    private val getDirectory =
+    private lateinit var editTextDownloadDir: TextInputEditText
+
+    private val setDownloadDirectory =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == Activity.RESULT_OK) {
                 // The result data contains a URI for the document or directory that
@@ -38,48 +38,27 @@ class MainActivity : AppCompatActivity() {
                         uri,
                         DocumentsContract.getTreeDocumentId(uri)
                     )
-
                     val fullFilePath = UriUtils.getPathFromUri(this, docUriTree)
-                    // val realPath = FileHelper.getUriRealPath(this, uri) ?: ""
-
                     SharedPreferenceHelper.addString(Constants.URL, fullFilePath)
-
                     val path = SharedPreferenceHelper.getString(Constants.URL, "")
-                    Toast.makeText(applicationContext, path, Toast.LENGTH_SHORT).show()
+
+                    editTextDownloadDir.setText(path)
                 }
             }
 
         }
-
-    fun getRealPathFromURI(context: Context, contentUri: Uri?): String? {
-        var cursor: Cursor? = null
-        contentUri?.let {
-            return try {
-                val proj = arrayOf(MediaStore.Images.Media.DATA)
-                cursor = context.contentResolver.query(contentUri, proj, null, null, null)
-                val column_index: Int = cursor!!.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-                cursor!!.moveToFirst()
-                cursor!!.getString(column_index)
-            } finally {
-                cursor?.close()
-            }
-        }
-        return null
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        editTextDownloadDir = findViewById(R.id.editTextDownloadFolder)
         findViewById<View>(R.id.btnSearch).setOnClickListener {
             val url = findViewById<TextInputEditText>(R.id.editTextUrl).text.toString()
             openWebsiteScannerScreen(url)
         }
-
         findViewById<View>(R.id.editTextDownloadFolder).setOnClickListener {
             val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
             i.addCategory(Intent.CATEGORY_DEFAULT)
-            getDirectory.launch(Intent.createChooser(i, "Choose directory"))
+            setDownloadDirectory.launch(Intent.createChooser(i, "Choose directory"))
         }
     }
 
