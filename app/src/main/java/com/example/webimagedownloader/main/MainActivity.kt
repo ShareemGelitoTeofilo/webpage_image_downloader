973ac16ccd27ce80ac114a483b9755611064f1c3
LAPTOP-9D7D05BF\Shareem <shareemgelitoteofilo@yahoo.com>
2022-01-15 00:04:20 -0800

    Added mvvm pattern to MainActivity

--- app/src/main/java/com/example/webimagedownloader/main/MainActivity.kt
+++ app/src/main/java/com/example/webimagedownloader/main/MainActivity.kt
@@ -17,46 +17,45 @@ import com.example.webimagedownloader.webscanning.WebsiteScanningActivity
 import com.google.android.material.textfield.TextInputEditText
 import android.provider.MediaStore
 import android.provider.DocumentsContract
+import androidx.lifecycle.ViewModelProvider
 import com.example.webimagedownloader.utils.UriUtils
 import com.example.webimagedownloader.utils.checkConnectivityAndExecute
 import com.example.webimagedownloader.utils.network.NetworkVariable
+import com.example.webimagedownloader.webscanning.WebScanningViewModel
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var editTextDownloadDir: TextInputEditText
+    private lateinit var viewModel: MainViewModel
 
     private val setDownloadDirectory =
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == Activity.RESULT_OK) {
-                // The result data contains a URI for the document or directory that
-                // the user selected.
                 it.data?.data?.also { uri ->
-                    // TODO move later on the view models
-                    val docUriTree = DocumentsContract.buildDocumentUriUsingTree(
-                        uri,
-                        DocumentsContract.getTreeDocumentId(uri)
-                    )
-                    val fullFilePath = UriUtils.getPathFromUri(this, docUriTree)
-                    SharedPreferenceHelper.addString(Constants.URL, fullFilePath)
-                    val path = SharedPreferenceHelper.getString(Constants.URL, "")
-
-                    editTextDownloadDir.setText(path)
+                    viewModel.processDownloadDirectory(uri, applicationContext)
                 }
             }
-
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.downloadPath.observe(this) {
+            editTextDownloadDir.setText(it)
+        }
+
         editTextDownloadDir = findViewById(R.id.editTextDownloadFolder)
+
         findViewById<View>(R.id.btnSearch).setOnClickListener {
             checkConnectivityAndExecute(applicationContext) {
                 val url = findViewById<TextInputEditText>(R.id.editTextUrl).text.toString()
                 openWebsiteScannerScreen(url)
             }
         }
+
         findViewById<View>(R.id.editTextDownloadFolder).setOnClickListener {
             val i = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
             i.addCategory(Intent.CATEGORY_DEFAULT)
---
+++ app/src/main/java/com/example/webimagedownloader/main/MainViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.webimagedownloader.main
+
+import android.content.Context
+import android.net.Uri
+import android.provider.DocumentsContract
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.webimagedownloader.utils.Constants
+import com.example.webimagedownloader.utils.SharedPreferenceHelper
+import com.example.webimagedownloader.utils.UriUtils
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    private val _downloadPath = MutableLiveData<String>()
+
+    val downloadPath: LiveData<String>
+        get() = _downloadPath
+
+    fun processDownloadDirectory(uri: Uri, context: Context) {
+        viewModelScope.launch {
+            val fullFilePath = getFullPath(uri, context)
+            savePathToPreferences(fullFilePath)
+            _downloadPath.value = fullFilePath
+        }
+    }
+
+    private fun savePathToPreferences(downloadPath: String) {
+        SharedPreferenceHelper.addString(Constants.URL, downloadPath)
+    }
+
+    private fun getFullPath(uri: Uri, context: Context): String {
+        val docUriTree = DocumentsContract.buildDocumentUriUsingTree(
+            uri,
+            DocumentsContract.getTreeDocumentId(uri)
+        )
+        return UriUtils.getPathFromUri(context, docUriTree)
+    }
+}
